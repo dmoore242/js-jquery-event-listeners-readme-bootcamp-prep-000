7c277a50a7533140aa37967690556fa1071114f2
Devin Moore <devin.moore@volken.org>
2017-12-16 19:06:13 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -19,10 +19,17 @@ function pressIt() {
   })
 }
 
+function submitIt(){
+  $("form").on("submit", function(){
+    window.alert("Your form is going to be submitted now.");
+  });
+}
+
 $(document).ready(function(){
 
 // call functions here
   getIt();
   frameIt();
   pressIt();
+  submitIt();
 });
